9f2e79eed47bf9fe4b2be0f8c2f0a969ae9497d8
Yuri Salgado <yuri.salgado@fretebras.com.br>
2022-04-23 18:44:18 -0300

    add json-server mocks with proxy + env local

--- src/app/core/services/hero.service.ts
+++ src/app/core/services/hero.service.ts
@@ -1,24 +1,47 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, of, tap } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Hero } from '../models/hero.model';
 import { MessagesService } from './messages.service';
-import { HEROES } from './mock-heroes';
+// import { HEROES } from './mock-heroes';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
-  constructor(private messageService: MessagesService) {}
+  private heroesUrl = `${environment.baseUrl}/heroes`;
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessagesService
+  ) {}
 
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+    return this.http
+      .get<Hero[]>(this.heroesUrl)
+      .pipe(tap((heroes) => this.log(`fetched ${heroes.length} heroes`)));
+
+    // const heroes = of(HEROES);
+    // this.log('fetched heroes');
+    // return heroes;
   }
 
   getHero(id: number): Observable<Hero> {
-    const hero = HEROES.find((hero) => hero.id === id)!;
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(hero);
+    return this.http
+      .get<Hero>(`${this.heroesUrl}/${id}`)
+      .pipe(
+        tap((hero) =>
+          this.log(`fetched head id=${hero.name} and name=${hero.name}`)
+        )
+      );
+
+    // const hero = HEROES.find((hero) => hero.id === id)!;
+    // this.log(`fetched hero id=${id}`);
+    // return of(hero);
+  }
+
+  private log(message: string): void {
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
